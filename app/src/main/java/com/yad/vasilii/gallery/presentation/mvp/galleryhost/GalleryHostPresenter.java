3a697f7876f6b0421d41ee0cc90e6ab06603e56a
kalter704 <lantratov.vasia@yandex.ru>
2018-05-05 20:29:40 +0300

    Поменял дефолтные категории Пофиксил сбрасывание фрагментов категорий, если категории не менялись

--- app/src/main/java/com/yad/vasilii/gallery/presentation/mvp/galleryhost/GalleryHostPresenter.java
+++ app/src/main/java/com/yad/vasilii/gallery/presentation/mvp/galleryhost/GalleryHostPresenter.java
@@ -5,6 +5,8 @@ import com.yad.vasilii.gallery.domain.*;
 import com.yad.vasilii.gallery.presentation.global.*;
 import com.yad.vasilii.gallery.presentation.mvp.global.*;
 
+import java.util.*;
+
 import javax.inject.*;
 
 @InjectViewState
@@ -14,16 +16,13 @@ public class GalleryHostPresenter extends BasePresenter<GalleryHostView> {
 
     private SchedulersProvider mSchedulersProvider;
 
+    private List<String> mCategories;
+
     @Inject
     public GalleryHostPresenter(SchedulersProvider schedulersProvider,
             GalleryHostInteractor galleryHostInteractor) {
         mSchedulersProvider = schedulersProvider;
         mGalleryHostInteractor = galleryHostInteractor;
-    }
-
-    @Override
-    protected void onFirstViewAttach() {
-        loadCategories();
     }
 
     public void onResume() {
@@ -35,8 +34,29 @@ public class GalleryHostPresenter extends BasePresenter<GalleryHostView> {
                 .add(mGalleryHostInteractor.getCategories().subscribeOn(mSchedulersProvider.io())
                         .observeOn(mSchedulersProvider.ui()).doOnSubscribe(
                                 (disposable) -> getViewState().showWait("Загрузка данных..."))
-                        .subscribe(categories -> getViewState().showFragments(categories),
-                                Throwable::printStackTrace));
+                        .subscribe(this::successLoadCategories, Throwable::printStackTrace));
+    }
+
+    private void successLoadCategories(List<String> categories) {
+        if (!isEqualCategories(mCategories, categories)) {
+            mCategories = categories;
+            getViewState().showFragments(categories);
+        }
+    }
+
+    private boolean isEqualCategories(List<String> c1, List<String> c2) {
+        if (c1 == null || c2 == null) {
+            return false;
+        }
+        if (c1.size() != c2.size()) {
+            return false;
+        }
+        for (int i = 0; i < c1.size(); i++) {
+            if (!c1.get(i).equals(c2.get(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
